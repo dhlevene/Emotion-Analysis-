9fe431df74a07b58ed2c0e0f71a65cfdd1abfd9f
dhlevene <dhlevene@knights.ucf.edu>
2018-02-12 03:18:03 -0500

    Initial progress
    All of the current work completed including:
    - successful integration of Affectiva's library
    - successful raw emotion detection from both recorded video and live capture

---
+++ Assets/PlayMovie.cs
@@ -0,0 +1,130 @@
+﻿// Unity derives Video File Input Component UI from this file
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+namespace Affdex
+{
+    public class PlayMovie : MonoBehaviour, IDetectorInput
+    {
+        //public MovieTexture movie;
+        public VideoClip videoToPlay;
+        private VideoPlayer videoPlayer;
+        private VideoSource videoSource;
+        private AudioSource audioSource;
+
+        public float sampleRate = 20;
+        private Texture2D t2d;
+
+        public Detector detector
+        {
+            get; private set;
+        }
+
+        public Texture Texture
+        {
+            get
+            {
+                return videoPlayer.texture;
+            }
+        }
+
+        void Start()
+        {
+            Application.runInBackground = true;
+            //movie.Play();
+            t2d = new Texture2D(checked((int)videoToPlay.width), checked((int)videoToPlay.height), TextureFormat.RGB24, false);
+            StartCoroutine(playVideo());
+            detector = GetComponent<Detector>();
+        }
+
+        IEnumerator playVideo()
+        {
+            videoPlayer = gameObject.AddComponent<VideoPlayer>();
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+            videoPlayer.playOnAwake = false;
+            audioSource.playOnAwake = false;
+
+            videoPlayer.source = VideoSource.VideoClip;
+            videoPlayer.clip = videoToPlay;
+            videoPlayer.Prepare();
+
+            while (!videoPlayer.isPrepared)
+            {
+                Debug.Log("Preparing Video");
+                yield return null;
+            }
+
+            Debug.Log("Done Preparing Video");
+
+            videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+            videoPlayer.EnableAudioTrack(0, true);
+            videoPlayer.SetTargetAudioSource(0, audioSource);
+
+            //Assign the Texture from Video to RawImage to be displayed
+            //image.texture = videoPlayer.texture;
+
+            videoPlayer.Play();
+            audioSource.Play();
+
+            Debug.Log("Playing Video");
+            /*while (videoPlayer.isPlaying)
+            {
+                Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+                yield return null;
+            }
+
+            Debug.Log("Done Playing Video");*/
+        }
+
+
+        void OnEnable()
+        {
+            if (!AffdexUnityUtils.ValidPlatform())
+                return;
+
+            if (sampleRate > 0)
+                StartCoroutine(SampleRoutine());
+        }
+
+        private IEnumerator SampleRoutine()
+        {
+            while (enabled)
+            {
+
+                yield return new WaitForSeconds(1 / sampleRate);
+                if (detector.IsRunning)
+                {
+                    ProcessFrame();
+                }
+            }
+        }
+
+        private void ProcessFrame()
+        {
+            if (this.videoPlayer != null)
+            {
+                RenderTexture rt = RenderTexture.GetTemporary(checked((int)videoToPlay.width), checked((int)videoToPlay.height), 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default, 1);
+                RenderTexture.active = rt;
+
+                //Copy the movie texture to the render texture
+                Graphics.Blit(videoPlayer.texture, rt);
+
+
+                //Read the render texture to our temporary texture
+                t2d.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+
+                //apply the bytes
+                t2d.Apply();
+
+                //Send to the detector
+                Frame frame = new Frame(t2d.GetPixels32(), t2d.width, t2d.height, Frame.Orientation.Upright, Time.realtimeSinceStartup);
+                detector.ProcessFrame(frame);
+
+                RenderTexture.ReleaseTemporary(rt);
+            }
+        }
+    }
+}
---
+++ Assets/PlayerEmotions.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Affdex;
+using System.Collections.Generic;
+
+public class PlayerEmotions : ImageResultsListener
+{
+    public float currentValence;
+    public float currentAnger;
+    public float currentSmile;
+    public float currentDisgust;
+    public float currentEyeClosure;
+    public float currentJoy;
+    public float currentInterocularDistance;
+    public float currentAttention;
+    public float currentEngagement;
+    public FeaturePoint[] featurePointsList;
+    public Slider zenSlider;
+
+    Transform playerIcon;
+    Transform player;
+    Image playerIconImage;
+
+    public override void onFaceFound(float timestamp, int faceId)
+    {
+        //setIcon(255);
+        Debug.Log("Found the face");
+    }
+
+    public override void onFaceLost(float timestamp, int faceId)
+    {
+        //setIcon(20);
+        currentValence = 0;
+        currentJoy = 0;
+        currentAnger = 0;
+        currentSmile = 0;
+        currentDisgust = 0;
+        currentEyeClosure = 0;
+        currentInterocularDistance = 0;
+        currentAttention = 0;
+        currentEngagement = 0;
+        Debug.Log("Lost the face");
+        //player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public override void onImageResults(Dictionary<int, Face> faces)
+    {
+        Debug.Log("Got face results");
+
+        foreach (KeyValuePair<int, Face> pair in faces)
+        {
+            int FaceId = pair.Key;  // The Face Unique Id.
+            Face face = pair.Value;    // Instance of the face class containing emotions, and facial expression values.
+
+            //Retrieve the Emotions Scores
+            face.Emotions.TryGetValue(Emotions.Valence, out currentValence);
+            face.Emotions.TryGetValue(Emotions.Anger, out currentAnger);
+            face.Emotions.TryGetValue(Emotions.Joy, out currentJoy);
+            face.Emotions.TryGetValue(Emotions.Disgust, out currentDisgust);
+            face.Emotions.TryGetValue(Emotions.Engagement, out currentEngagement);
+
+            //Retrieve the Smile Score
+            face.Expressions.TryGetValue(Expressions.Smile, out currentSmile);
+            face.Expressions.TryGetValue(Expressions.EyeClosure, out currentEyeClosure);
+            face.Expressions.TryGetValue(Expressions.Attention, out currentAttention);
+
+            //Retrieve the Interocular distance, the distance between two outer eye corners.
+            currentInterocularDistance = face.Measurements.interOcularDistance;
+
+
+            //Retrieve the coordinates of the facial landmarks (face feature points)
+            featurePointsList = face.FeaturePoints;
+        }
+    }
+
+/*    private void setIcon(byte alpha)
+    {
+        playerIcon = GameObject.FindGameObjectWithTag("PlayerIcon").transform;
+        playerIconImage = playerIcon.GetComponent<Image>();
+        playerIconImage.color = new Color32(255, 255, 255, alpha);
+    }*/
+}
