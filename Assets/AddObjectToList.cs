33bb26dccdf31f5ac6e019910ca1b78a3d7e0af6
Jordan Zhu <qwertybot1@gmail.com>
2018-04-02 12:54:39 -0400

    added ui for label system
    stuff

---
+++ Assets/AddObjectToList.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddObjectToList : MonoBehaviour {
+
+    private int index = 0;
+
+    public Text timerText;
+
+    public GameObject itemTemplate;
+
+    public GameObject content;
+
+    
+
+    public void AddButtonClick()
+    {
+        var copy = Instantiate(itemTemplate);
+        copy.transform.SetParent(content.transform, false);
+        copy.GetComponentInChildren<Text>().text = timerText.text;
+
+        int copyOfIndex = index;
+
+        copy.GetComponent<Button>().onClick.AddListener(
+            () =>
+            {
+                // to-do: anything that happens at the current index
+                Debug.Log("Index number: " + copyOfIndex);
+            }
+            );
+        index++;
+    }
+}
